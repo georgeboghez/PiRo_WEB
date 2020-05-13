dcd14d58fd340c6eed98a3f02358e69af539bd91
radup99 <61940587+radup99@users.noreply.github.com>
2020-05-13 17:12:26 +0300

    added server

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const { Router } = require('../utils/Router')
+const indexController = require('../controller/index')
+// const aboutController = require('../controller/about')
+// const statisticsController = require('../controller/statistics')
+
+var router = new Router()
+
+
+router.get('/exemplu', indexController.exempleAPI)
+router.get('/', indexController.getIndexHTML)
+router.get('/index', indexController.getIndexHTML)
+router.get('/index.html', indexController.getIndexHTML)
+router.get('/css/header.css', indexController.getCSS)
+router.get('/css/footer.css', indexController.getCSS)
+router.get('/css/page-content.css', indexController.getCSS)
+router.get('/css/main-page.css', indexController.getCSS)
+router.get('/assets/img/romania.svg', indexController.getSVG)
+router.get('/assets/img/ag.svg', indexController.getSVG)
+router.get('/assets/img/school.svg', indexController.getSVG)
+router.get('/assets/img/dice.svg', indexController.getSVG)
+router.get('/chart-test', indexController.getChartHTML)
+router.get('/custom-statistics', indexController.getCustomStatsHTML)
+router.get('/css/custom-statistics.css', indexController.getCSS)
+router.get('/assets/img/graph.png', indexController.getSVG)
+
+// router.get('about.html', aboutController.getHTML)
+
+// router.get('statistics.html', statisticsController.getHTML)
+
+///exemplu
+///router.get(url_string, functieApelata)
+///
+module.exports.index = router
