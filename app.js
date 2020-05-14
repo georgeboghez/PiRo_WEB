7fa9558a63d3bd58eee1f38c0977a73c4553cdb9
TanaseTeofil <45397163+TanaseTeofil@users.noreply.github.com>
2020-05-14 19:07:46 +0300

    routes added for contact us page and about page

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const { Router } = require('./utils/Router')
 const { WebApp } = require('./utils/WebApp')
 const constants = require('./utils/constants')
 const { index } = require('./routes/index')
-
+const { contactus } = require('./routes/contactus')
+const { about } = require('./routes/about')
 const router = new Router()
 router.use('', index)
+router.use('', contactus)
+router.use('', about)
 console.log(router)
 
 const app = new WebApp(router)
---
+++ routes/contactus.js
@@ -0,0 +1,25 @@
+const { Router } = require('../utils/Router')
+const contactusController = require('../controller/contactus')
+// const aboutController = require('../controller/about')
+// const statisticsController = require('../controller/statistics')
+
+var router = new Router()
+
+router.get('/contact-us', contactusController.getContactHTML)
+router.get('/contact-us.html', contactusController.getContactHTML)
+router.get('/css/header.css', contactusController.getCSS)
+router.get('/css/footer.css', contactusController.getCSS)
+router.get('/css/contact-us.css', contactusController.getCSS)
+router.get('/assets/img/document.png', contactusController.getPNG)
+router.get('/assets/img/questionmark.png', contactusController.getPNG)
+router.get('/assets/img/suitcase.png', contactusController.getPNG)
+router.get('/assets/img/top.png', contactusController.getPNG)
+
+// router.get('about.html', aboutController.getHTML)
+
+// router.get('statistics.html', statisticsController.getHTML)
+
+///exemplu
+///router.get(url_string, functieApelata)
+///
+module.exports.contactus = router
