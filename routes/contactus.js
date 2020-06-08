c4165842226cbcd116519aa575af4f5b5055d1ed
radup99 <61940587+radup99@users.noreply.github.com>
2020-06-08 19:48:28 +0300

    refactored code, created a single Controller class

--- routes/contactus.js
+++ routes/contactus.js
@@ -1,21 +1,21 @@
 const { Router } = require('../utils/Router')
-const contactusController = require('../controller/contactus')
+const { Controller } = require('../controller/Controller')
 
 var router = new Router()
+var controller = new Controller()
 
-router.get('/contact-us', contactusController.getContactHTML)
-router.get('/contact-us.html', contactusController.getContactHTML)
-router.get('/css/header.css', contactusController.getCSS)
-router.get('/css/footer.css', contactusController.getCSS)
-router.get('/css/contact-us.css', contactusController.getCSS)
-router.get('/assets/img/docs.svg', contactusController.getSVG)
-router.get('/assets/img/questionmark.svg', contactusController.getSVG)
-router.get('/assets/img/suitcase.svg', contactusController.getSVG)
-router.get('/js/contact-us.js', contactusController.getJS)
-router.get('/js/header.js', contactusController.getJS)
-router.get('/captcha', contactusController.getCaptcha)
+router.get('/contact-us.html', controller.getHTML)
+router.get('/css/header.css', controller.getCSS)
+router.get('/css/footer.css', controller.getCSS)
+router.get('/css/contact-us.css', controller.getCSS)
+router.get('/assets/img/docs.svg', controller.getSVG)
+router.get('/assets/img/questionmark.svg', controller.getSVG)
+router.get('/assets/img/suitcase.svg', controller.getSVG)
+router.get('/js/contact-us.js', controller.getJS)
+router.get('/js/header.js', controller.getJS)
+router.get('/captcha', controller.getCaptcha)
 
-router.post('/sendMail', contactusController.sendMailtoFront)
+router.post('/sendMail', controller.sendMailtoFront)
 
 
 module.exports.contactus = router
--- utils/controllerUtils.js
+++ utils/controllerUtils.js
@@ -7,5 +7,36 @@ module.exports = {
 				resolve(body);
 			});
 		})
+	},
+
+	isEmailValid: function isEmailValid(email) {
+		var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+		if (!email)
+			return false;
+		if (email.length > 254)
+			return false;
+		var valid = emailRegex.test(email);
+		if (!valid)
+			return false;
+		var parts = email.split("@");
+		if (parts[0].length > 64)
+			return false;
+		var domainParts = parts[1].split(".");
+		if (domainParts.some(function(part) { return part.length > 63; }))
+			return false;
+		return true;
+	},
+
+	isNameValid: function isNameValid(name) {
+		if (!name)
+			return false;
+		if (name.length > 254)
+			return false;
+		return true;
+	},
+
+	isCaptchaTextCorrect: function isCaptchaTextCorrect(captcha_text, captchaText) {
+		return captchaText == captcha_text;
 	}
+
 }
